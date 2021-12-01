673e300f90bd0b820e65b2e3af28c3e27f6458fb
David Mihola <xmihol00@vutbr.cz>
2021-12-01 16:42:52 +0100

    doc

--- src/UART.c
+++ src/UART.c
@@ -25,12 +25,12 @@ static void IRAM_ATTR uart_interupt_handler(void *arg)
     uint16_t rx_fifo_len;
 
 	UART0.status.ctsn;
-    rx_fifo_len = UART0.status.rxfifo_cnt; // read number of bytes in UART buffer
+    rx_fifo_len = UART0.status.rxfifo_cnt; // ziskani poctu bytu v bufferu UART
     
     while(rx_fifo_len--)
     {
         recieve_buffer[++recieve_buffer_pos] = UART0.fifo.rw_byte;
-		if (wifi_status == WIFI_PASSWORD)
+		if (wifi_status == WIFI_PASSWORD) // zadavani wifi hesla
 		{
 			if (recieve_buffer[recieve_buffer_pos] == '\r')
 			{
@@ -43,7 +43,7 @@ static void IRAM_ATTR uart_interupt_handler(void *arg)
 				uart_write_bytes(ACTIVE_UART, "*", 1);
 			}
 		}
-		else if (wifi_status == WIFI_UNAME)
+		else if (wifi_status == WIFI_UNAME) // zadavani wifi ssid
 		{
 			if (recieve_buffer[recieve_buffer_pos] == '\r')
 			{
@@ -63,25 +63,26 @@ static void IRAM_ATTR uart_interupt_handler(void *arg)
 
 		if (recieve_buffer[recieve_buffer_pos] == 0x08 || recieve_buffer[recieve_buffer_pos] == 0x7f) // backspace
 		{
-			uart_write_bytes(ACTIVE_UART, "\b \b", 3);
+			uart_write_bytes(ACTIVE_UART, "\b \b", 3); // zmazani posledniho znaku
 			recieve_buffer_pos = recieve_buffer_pos < 2 ?  UINT8_MAX : recieve_buffer_pos - 2;
 		}
 
-		if (recieve_buffer[recieve_buffer_pos] == '\r')
+		if (recieve_buffer[recieve_buffer_pos] == '\r') // konec radku
 		{
-			uart_clear_intr_status(ACTIVE_UART, UART_RXFIFO_FULL_INT_CLR|UART_RXFIFO_TOUT_INT_CLR);
+			uart_clear_intr_status(ACTIVE_UART, UART_RXFIFO_FULL_INT_CLR|UART_RXFIFO_TOUT_INT_CLR); // potvrzeni preruseni
 			recieve_buffer[recieve_buffer_pos] = '\n';
 			uart_write_bytes(ACTIVE_UART, &recieve_buffer[recieve_buffer_pos], 1);
-			xTaskCreate(&parse_input, "parse_input", 4096, NULL, 5, &parse_handle);
+			xTaskCreate(&parse_input, "parse_input", 4096, NULL, 5, &parse_handle); // vytvoreni systemove ulohy pro zpracovani vstupu
 			return;
 		}
     }
 	
-    uart_clear_intr_status(ACTIVE_UART, UART_RXFIFO_FULL_INT_CLR|UART_RXFIFO_TOUT_INT_CLR);
+    uart_clear_intr_status(ACTIVE_UART, UART_RXFIFO_FULL_INT_CLR|UART_RXFIFO_TOUT_INT_CLR); // potvrzeni preruseni
 }
 
 void init_uart()
 {
+	// nastaveni konfigurace UART
     uart_config_t uart_config = 
     {
 		.baud_rate = UART_BAUD_RATE,
@@ -91,42 +92,41 @@ void init_uart()
 		.flow_ctrl = UART_HW_FLOWCTRL_DISABLE
 	};
 
-	ESP_ERROR_CHECK(uart_param_config(ACTIVE_UART, &uart_config));
+	ESP_ERROR_CHECK(uart_param_config(ACTIVE_UART, &uart_config)); // konfigurace UART
 
-	//Set UART pins (using UART0 default pins ie no changes.)
-	ESP_ERROR_CHECK(uart_set_pin(ACTIVE_UART, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+	ESP_ERROR_CHECK(uart_set_pin(ACTIVE_UART, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE)); // nastaveni vstupu
+	
+	ESP_ERROR_CHECK(uart_driver_install(ACTIVE_UART, BUFFER_SIZE << 1, 0, 0, NULL, 0)); // nastaveni bufferu pro prijem dat
+	
+	ESP_ERROR_CHECK(uart_isr_free(ACTIVE_UART)); // vynulovani preruseni
 
-	//Install UART driver, and get the queue.
-	ESP_ERROR_CHECK(uart_driver_install(ACTIVE_UART, BUFFER_SIZE << 1, 0, 0, NULL, 0));
-
-	// release the pre registered UART handler/subroutine
-	ESP_ERROR_CHECK(uart_isr_free(ACTIVE_UART));
-
-	// register new UART subroutine
-	ESP_ERROR_CHECK(uart_isr_register(ACTIVE_UART, uart_interupt_handler, NULL, ESP_INTR_FLAG_IRAM, &handle_console));
-
-	// enable RX interrupt
-	ESP_ERROR_CHECK(uart_enable_rx_intr(ACTIVE_UART));
+	ESP_ERROR_CHECK(uart_isr_register(ACTIVE_UART, uart_interupt_handler, NULL, ESP_INTR_FLAG_IRAM, &handle_console)); // nasataveni handleru pro preruseni
+	
+	ESP_ERROR_CHECK(uart_enable_rx_intr(ACTIVE_UART)); // povoleni preruseni
 }
 
 void uart_print_measurment(measurment_t *measurment)
 {
 	struct tm local_time;
+	// ziskani ulozeneho casu
 	localtime_r(&measurment->time, &local_time);
+	// formatovani namerenych dat
 	sprintf(&send_buffer[strftime(send_buffer, BUFFER_SIZE, "| %c", &local_time) - 5], ":       \t     %3.3f °C\t|\r\n", measurment->temperature);
 
-	uart_write_bytes(ACTIVE_UART, send_buffer, strlen(send_buffer));
-	vTaskDelay(25 / portTICK_RATE_MS);
+	uart_write_bytes(ACTIVE_UART, send_buffer, strlen(send_buffer)); // vypis
+	vTaskDelay(25 / portTICK_RATE_MS); // po vytisku cekani 25 ms pro uvolneni UART buffer
 }
 
 void uart_log_measurment(measurment_t *measurment)
 {
 	struct tm local_time;
+	// ziskani ulozeneho casu
 	localtime_r(&measurment->time, &local_time);
+	// formatovani namerenych dat
 	sprintf(&send_buffer[strftime(send_buffer, BUFFER_SIZE, "* log: %c", &local_time) - 5], ": %.3f °C\r\n", measurment->temperature);
 
-	uart_write_bytes(ACTIVE_UART, send_buffer, strlen(send_buffer));
-	vTaskDelay(25 / portTICK_RATE_MS);
+	uart_write_bytes(ACTIVE_UART, send_buffer, strlen(send_buffer)); // vypis
+	vTaskDelay(25 / portTICK_RATE_MS); // po vytisku cekani 25 ms pro uvolneni UART buffer
 }
 
 static void parse_input()
@@ -136,30 +136,31 @@ static void parse_input()
 	recieve_buffer[++recieve_buffer_pos] = '\0';
 
 	recieve_buffer_pos = 0;
-	skip_buffer_spaces();
+	skip_buffer_spaces(); // preskoceni pocatecnich bilych znaku
 
-	if (wifi_status == WIFI_UNAME)
+	if (wifi_status == WIFI_UNAME) // zpracovani ssid wifi
 	{
 		parsed = strlen(recieve_buffer);
-		strncpy(credentials.name, recieve_buffer, parsed >= CREDENTIAL_NAME ? CREDENTIAL_NAME - 1 : parsed);
+		strncpy(credentials.name, recieve_buffer, parsed >= CREDENTIAL_NAME ? CREDENTIAL_NAME - 1 : parsed); // ulozeni ssid do prihl. udaju
 		wifi_status = WIFI_PASSWORD;
 		uart_print_string("password: ");
 	}
-	else if (wifi_status == WIFI_PASSWORD)
+	else if (wifi_status == WIFI_PASSWORD) // zpracovani hesla wifi
 	{
 		parsed = strlen(recieve_buffer);
-		strncpy(credentials.password, recieve_buffer, parsed >= CREDENTIAL_PASS ? CREDENTIAL_PASS - 1 : parsed);
+		strncpy(credentials.password, recieve_buffer, parsed >= CREDENTIAL_PASS ? CREDENTIAL_PASS - 1 : parsed); // ulozeni hesla do prihlas. udaju
 		wifi_status = NO_WIFI;
-		store_credentials();
+		store_credentials(); // zmena udaju
 
 		uart_print_string("Changing credentials ...\r\n");
-		wifi_disconnect();
+		// reconnect k wifi
+		wifi_disconnect(); 
 		wifi_connect();
 		
 		vTaskDelay(50 / portTICK_RATE_MS);
 		print_status();
 	}
-	else if (recieve_buffer[recieve_buffer_pos] == 's')
+	else if (recieve_buffer[recieve_buffer_pos] == 's') // s|status
 	{
 		recieve_buffer_pos++;
 		if (isspace(recieve_buffer[recieve_buffer_pos]) || 
@@ -169,10 +170,10 @@ static void parse_input()
 		}
 		else
 		{
-			unrecognized = 3;
+			unrecognized = 3; // spatny prikaz
 		}
 	}
-	else if (recieve_buffer[recieve_buffer_pos] == 'p')
+	else if (recieve_buffer[recieve_buffer_pos] == 'p') // p|print
 	{
 		recieve_buffer_pos++;
 		if (isspace(recieve_buffer[recieve_buffer_pos]) || 
@@ -180,10 +181,10 @@ static void parse_input()
 		{
 			skip_buffer_spaces();
 
-			parsed = atoi(&recieve_buffer[recieve_buffer_pos]);
+			parsed = atoi(&recieve_buffer[recieve_buffer_pos]); // ziskani poctu pro tisk
 			if (parsed <= 0)
 			{
-				unrecognized = 2;
+				unrecognized = 2; // spatna hodnota prikazu
 			}
 			else
 			{
@@ -205,7 +206,7 @@ static void parse_input()
 			unrecognized = 3;
 		}
 	}
-	else if (recieve_buffer[recieve_buffer_pos] == 'w')
+	else if (recieve_buffer[recieve_buffer_pos] == 'w') //w|wifi
 	{
 		recieve_buffer_pos++;
 		if (isspace(recieve_buffer[recieve_buffer_pos]) || 
@@ -213,32 +214,30 @@ static void parse_input()
 		{
 			skip_buffer_spaces();
 
-			if (!strncmp(&recieve_buffer[recieve_buffer_pos], "connect", 7) && isspace(recieve_buffer[recieve_buffer_pos += 7]))
+			if (!strncmp(&recieve_buffer[recieve_buffer_pos], "connect", 7) && isspace(recieve_buffer[recieve_buffer_pos += 7])) // pripojeni k wifi
 			{
 				uart_print_string("Connecting ...\r\n");
-				if (!wifi_is_connected())
+				if (!wifi_is_connected()) // puze pokud je odpojena
 				{
 					wifi_connect();
 				}
 				vTaskDelay(50 / portTICK_RATE_MS);
 				print_status();
 			}
-			else if (!strncmp(&recieve_buffer[recieve_buffer_pos], "disconnect", 10) && isspace(recieve_buffer[recieve_buffer_pos += 10]))
+			else if (!strncmp(&recieve_buffer[recieve_buffer_pos], "disconnect", 10) && isspace(recieve_buffer[recieve_buffer_pos += 10])) // odpojeni wifi
 			{
 				uart_print_string("Disconnecting ...\r\n");
-				if (wifi_is_connected())
+				if (wifi_is_connected()) // pouze pokud je odpojena
 				{
 					wifi_disconnect();
 				}
 				print_status();
-				
 			}
-			else if (!strncmp(&recieve_buffer[recieve_buffer_pos], "auth", 4) && isspace(recieve_buffer[recieve_buffer_pos += 4]))
+			else if (!strncmp(&recieve_buffer[recieve_buffer_pos], "auth", 4) && isspace(recieve_buffer[recieve_buffer_pos += 4])) // autentizace wifi
 			{
-				wifi_status = WIFI_UNAME;
-				memset(&credentials, 0, CREDENTIAL_SIZE);
+				wifi_status = WIFI_UNAME; // nastaveni autentizacniho cyklu
+				memset(&credentials, 0, CREDENTIAL_SIZE); // vynulovani puvodnich udaju
 				uart_print_string("name: ");
-				
 			}
 			else
 			{
@@ -250,7 +249,7 @@ static void parse_input()
 			unrecognized = 3;
 		}
 	}
-	else if (recieve_buffer[recieve_buffer_pos] == 'l')
+	else if (recieve_buffer[recieve_buffer_pos] == 'l') // l|log
 	{
 		recieve_buffer_pos++;
 		if (isspace(recieve_buffer[recieve_buffer_pos]) || 
@@ -258,16 +257,16 @@ static void parse_input()
 		{
 			skip_buffer_spaces();
 
-			if (recieve_buffer[recieve_buffer_pos] == '0' && isspace(recieve_buffer[recieve_buffer_pos + 1]))
+			if (recieve_buffer[recieve_buffer_pos] == '0' && isspace(recieve_buffer[recieve_buffer_pos + 1])) // vypnuti logovani
 			{
 				unrecognized = set_log_interval(0, 0);
 			}
-			else
+			else // zapnuti logovani
 			{
-				parsed = atoi(&recieve_buffer[recieve_buffer_pos]);
+				parsed = atoi(&recieve_buffer[recieve_buffer_pos]); // ziskani hodnoty pro periodu logovani
 				if (parsed <= 0)
 				{
-					unrecognized = 2;
+					unrecognized = 2; // spatna hodnota
 				}
 				else
 				{
@@ -276,7 +275,7 @@ static void parse_input()
 
 					if (isspace(recieve_buffer[recieve_buffer_pos + 1]))
 					{
-						unrecognized = set_log_interval((uint32_t)parsed, recieve_buffer[recieve_buffer_pos]);
+						unrecognized = set_log_interval((uint32_t)parsed, recieve_buffer[recieve_buffer_pos]); // nastaveni intervalu logovani
 					}
 					else
 					{
@@ -287,10 +286,10 @@ static void parse_input()
 		}
 		else
 		{
-			unrecognized = 3;
+			unrecognized = 3; // nerozpoznany prikaz
 		}
 	}
-	else if (recieve_buffer[recieve_buffer_pos] == 'h')
+	else if (recieve_buffer[recieve_buffer_pos] == 'h') // h|help
 	{
 		recieve_buffer_pos++;
 		if (isspace(recieve_buffer[recieve_buffer_pos]) || 
@@ -327,7 +326,7 @@ static void parse_input()
 			unrecognized = 3;
 		}
 	}
-	else if (recieve_buffer[recieve_buffer_pos] == 't')
+	else if (recieve_buffer[recieve_buffer_pos] == 't') // t|time
 	{
 		recieve_buffer_pos++;
 		if (isspace(recieve_buffer[recieve_buffer_pos]) || 
@@ -352,10 +351,10 @@ static void parse_input()
 	}
 	else if (recieve_buffer[recieve_buffer_pos - 1] != '\n')
 	{
-		unrecognized = 1;
+		unrecognized = 1; // nerozpoznany prikaz
 	}
 
-	if (unrecognized)
+	if (unrecognized) // nerozpoznano
 	{
 		sprintf(recieve_buffer, "Command was not recognized.\r\nType 'h' or 'help' on a single line to view the help message.\r\n");
 		uart_write_bytes(ACTIVE_UART, recieve_buffer, strlen(recieve_buffer));
@@ -384,7 +383,7 @@ static void skip_buffer_characters()
 void uart_print_string(const char * string)
 {
 	uart_write_bytes(ACTIVE_UART, string, strlen(string));
-	vTaskDelay(25 / portTICK_RATE_MS);
+	vTaskDelay(25 / portTICK_RATE_MS); // vyckani po tisknuti
 }
 
 void print_status()
@@ -395,12 +394,15 @@ void print_status()
 	time_t time_sec = get_start_time();
 	struct tm local_time;
 	uint8_t lenght = 0;
+
+	// formatovani casu spusteni zarizeni
 	localtime_r(&time_sec, &local_time);
 	sprintf(send_buffer, "| - Temperature measuring started at ");
 	lenght = strlen(send_buffer);
 	strftime(&send_buffer[lenght], BUFFER_SIZE - lenght, "%c.\t\t\t|\r\n", &local_time);
 	uart_print_string(send_buffer);
 
+	// formatovani casu na zarizeni
 	time(&time_sec);
 	localtime_r(&time_sec, &local_time);
 	sprintf(send_buffer, "| - Current time on the device is ");
@@ -408,6 +410,7 @@ void print_status()
 	strftime(&send_buffer[lenght], BUFFER_SIZE - lenght, "%c.\t\t\t|\r\n", &local_time);
 	uart_print_string(send_buffer);
 
+	// formatovani casu posledni synchronizace
 	time_sec = get_last_sync();
 	localtime_r(&time_sec, &local_time);
 	sprintf(send_buffer, "| - Last synchronization of time carried out at ");
@@ -422,7 +425,7 @@ void print_status()
 																							 s_ip_addr.addr & 0xFF,
 																							 (s_ip_addr.addr >> 8) & 0xFF, 
 																							 (s_ip_addr.addr >> 16) & 0xFF,
-																							 s_ip_addr.addr >> 24);
+																							 s_ip_addr.addr >> 24); // formtovani IP adresy
 		uart_print_string(send_buffer);	
 	}
 	else
--- src/WiFi.c
+++ src/WiFi.c
@@ -19,6 +19,7 @@ credentials_t credentials;
 
 static void signalize_wifi_disconnected()
 {
+    // rozsviceni LED diody
     gpio_pad_select_gpio(GPIO_LED_RED);
     gpio_set_direction(GPIO_LED_RED, GPIO_MODE_OUTPUT);
     gpio_set_level(GPIO_LED_RED, 1);
@@ -26,13 +27,14 @@ static void signalize_wifi_disconnected()
 
 static void signalize_wifi_connected()
 {
+    // zhasnuti LED diody
     gpio_set_level(GPIO_LED_RED, 0);
     gpio_set_direction(GPIO_LED_RED, GPIO_MODE_DISABLE);
 }
 
 esp_err_t wifi_connect()
 {
-    signalize_wifi_disconnected();
+    signalize_wifi_disconnected(); // zignalizace LED diodou, ze wifi neni pripojena
     s_ip_addr.addr = 0;
     connected = false;
 
@@ -42,19 +44,19 @@ esp_err_t wifi_connect()
     }
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
-    esp_event_loop_create_default();
+    esp_event_loop_create_default(); // vytvoreni obsluzne rutiny pro zpracovani WiFi udalosti
 
     wifi_start();
-    ESP_ERROR_CHECK(esp_register_shutdown_handler(&wifi_stop));
+    ESP_ERROR_CHECK(esp_register_shutdown_handler(&wifi_stop)); // registrace handleru pro vypnuti wifi
 
     for (int i = 0; i < s_active_interfaces; i++) 
     {
-        xSemaphoreTake(s_semph_get_ip_addrs, portMAX_DELAY);
+        xSemaphoreTake(s_semph_get_ip_addrs, portMAX_DELAY); 
     }
 
     if (s_ip_addr.addr == 0)
     {
-        wifi_disconnect();
+        wifi_disconnect(); // pripojeni se nezdarilo, wifi je vhodne odpojit
     }
     
     return ESP_OK;
@@ -68,36 +70,34 @@ esp_err_t wifi_disconnect()
     }
     vSemaphoreDelete(s_semph_get_ip_addrs);
     s_semph_get_ip_addrs = NULL;
-    wifi_stop();
-    ESP_ERROR_CHECK(esp_unregister_shutdown_handler(&wifi_stop));
+    wifi_stop(); // zastaveni wifi
+    ESP_ERROR_CHECK(esp_unregister_shutdown_handler(&wifi_stop)); // zruseni handleru pro vypnuti
 
-    signalize_wifi_disconnected();
+    signalize_wifi_disconnected(); // rozsviceni diody
     return ESP_OK;
 }
 
 static void on_wifi_disconnect(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
-    ESP_LOGI("WIFI", "%d", s_ip_addr.addr);
-    if (s_ip_addr.addr == 0)
+    if (s_ip_addr.addr == 0) // wifi nebyla pripojena
     {
-        ESP_LOGI("WIFI", "SEM givup");
-        xSemaphoreGive(s_semph_get_ip_addrs);
+        xSemaphoreGive(s_semph_get_ip_addrs); // uvolneni semaforu a pokracovani bez pripojeni
     }
     else
     {
-        signalize_wifi_disconnected();
-        connected = false;
-        esp_wifi_connect();
+        signalize_wifi_disconnected(); // zignalizace LED diodou, ze doslo k odpojeni
+        connected = false; // wifi neni pripojena
+        esp_wifi_connect(); // pokud o opetovne pripojeni
     }    
 }
 
 static void wifi_start()
 {
     char *desc;
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); // ziskani zakladni konfigurace
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg)); // inicializace wifi
 
-    esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_STA();
+    esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_STA(); // ziskani zakladni konfigurace
     
     asprintf(&desc, "%s: %s", TAG, esp_netif_config.if_desc);
     esp_netif_config.if_desc = desc;
@@ -106,6 +106,7 @@ static void wifi_start()
     free(desc);
     esp_wifi_set_default_wifi_sta_handlers();
 
+    // registrace hanfdleru pro nahle odpojeni a pro pripojeni (ziskani IP adresy)
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip, NULL));
 
@@ -116,9 +117,11 @@ static void wifi_start()
             .password = "",
         },
     };
-    memcpy(wifi_config.sta.ssid, credentials.name, CREDENTIAL_NAME);
+    // vyplneni prihlasovacich udaju
+    memcpy(wifi_config.sta.ssid, credentials.name, CREDENTIAL_NAME); 
     memcpy(wifi_config.sta.password, credentials.password, CREDENTIAL_PASS);
 
+    // nastaveni a spusteni wifi
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
@@ -132,10 +135,11 @@ static void wifi_start()
 static void wifi_stop()
 {
     esp_netif_t *wifi_netif = get_netif_from_desc("sta");
+    // odebrani handleru pro odpojeni a ziskani IP
     ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect));
     ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip));
 
-    esp_err_t err = esp_wifi_stop();
+    esp_err_t err = esp_wifi_stop(); // zastaveni wifi
     if (err == ESP_ERR_WIFI_NOT_INIT) 
     {
         return;
@@ -146,6 +150,7 @@ static void wifi_stop()
     }
 
     ESP_ERROR_CHECK(err);
+    // deinicializace wifi
     ESP_ERROR_CHECK(esp_wifi_deinit());
     ESP_ERROR_CHECK(esp_wifi_clear_default_wifi_driver_and_handlers(wifi_netif));
     esp_netif_destroy(wifi_netif);
